1da78e9e1b99796a0c6c03cd5745a1e2060ac5e2
crapper <crapper.612@gmail.com>
2023-05-23 20:41:35 +0800

    refactor: remove NewScale, replaces as ttk Scale, remove tk optionmenu, replace as ttk combobox

--- app/config_page.py
+++ app/config_page.py
@@ -2,53 +2,6 @@ import tkinter as tk
 
 from .page import *
 
-import locale
-locale.setlocale(locale.LC_NUMERIC, 'pl_PL.UTF8')
-
-class NewScale(tk.Frame):
-    def __init__(self, master=None, **options):
-        tk.Frame.__init__(self, master)
-
-        # Disable normal value display...
-        options['showvalue'] = False
-        # ... and use custom display instead
-        options['command'] = self._on_scale
-
-        # Set resolution to 1 and adjust to & from value 
-        self.res = options.get('resolution', 1)
-        from_ = int(options.get('from_', 0) / self.res)
-        to = int(options.get('to', 100) / self.res)
-        options.update({'resolution': 1, 'to': to, 'from_': from_})
-
-        # This could be improved...
-        if 'digits' in options:
-            self.digits = ['digits']
-            del options['digits']
-        else:
-            self.digits = 2
-
-        self.scale = tk.Scale(self, **options)
-        self.scale_label = tk.Label(self)
-        orient = options.get('orient', tk.VERTICAL)
-        if orient == tk.VERTICAL:
-            side, fill = 'right', 'y'
-        else:
-            side, fill = 'top', 'x'
-        self.scale.pack(side=side, fill=fill)
-        self.scale_label.pack(side=side)
-
-    def _on_scale(self, value):
-        value = locale.atof(value) * self.res
-        value = locale.format_string('%.*f', (self.digits, value))
-        self.scale_label.configure(text=value)
-
-    def get(self):
-        return self.scale.get() * self.res
-
-    def set(self, value):   
-        self.scale.set(int(0.5 + value / self.res))
-
-
 class ConfigPage(Page):
     def __init__(self, parent: tk.Canvas, controller: App):
         Page.__init__(self, parent, controller)
@@ -74,14 +27,14 @@ class ConfigPage(Page):
         options = [0, 1, 2]
         clicked = tk.IntVar()
         clicked.set(options[1])
-        self.drop_model_complexity = tk.OptionMenu(self.controller, clicked , *options)
+        self.drop_model_complexity = tk.ttk.Combobox(self.controller, values=options, state="readonly", width=2, font=("Helvetica", 10))
         self.drop_model_complexity.place(x= self.BgTop_x+int((self.BgDown_x-self.BgTop_x)*0.1) + width_model_complexity, y = self.BgTop_y+int((self.BgDown_y-self.BgTop_y)*0.1+height_weight))
         self.drop_model_complexity.place_forget()
 
         #Drag bar for model confidence
         self.panel.append(self.parent.create_text(self.BgTop_x+int((self.BgDown_x-self.BgTop_x)*0.1), self.BgTop_y+int((self.BgDown_y-self.BgTop_y)*0.1+height_weight+height_model_complexity), text="ModelConf: ", font=("Helvetica", 16), fill="black", anchor=tk.NW)) 
         width_model_conf, height_model_conf = self.get_width_height(self.panel[4])
-        self.drag_model_conf = NewScale(self.controller, from_=0, to=1, orient=tk.HORIZONTAL, tickinterval=0.1, resolution =0.01, width=5)
+        self.drag_model_conf = tk.ttk.Scale(self.controller, from_=0, to=1, orient="horizontal")
         self.drag_model_conf.set(0.5)
         self.drag_model_conf.place(x= self.BgTop_x+int((self.BgDown_x-self.BgTop_x)*0.1) + width_model_conf, y = self.BgTop_y+int((self.BgDown_y-self.BgTop_y)*0.1+height_weight+height_model_complexity))
         self.drag_model_conf.place_forget()
@@ -89,7 +42,7 @@ class ConfigPage(Page):
         #Drag bar for tracking confidence
         self.panel.append(self.parent.create_text(self.BgTop_x+int((self.BgDown_x-self.BgTop_x)*0.1), self.BgTop_y+int((self.BgDown_y-self.BgTop_y)*0.1+height_weight+height_model_complexity+height_model_conf+16), text="TrackingConf: ", font=("Helvetica", 16), fill="black", anchor=tk.NW))
         width_track_conf, height_track_conf = self.get_width_height(self.panel[5])
-        self.drag_track_conf = NewScale(self.controller, from_=0, to=1, orient=tk.HORIZONTAL, tickinterval=0.1, resolution =0.01, width=5)
+        self.drag_track_conf = tk.ttk.Scale(self.controller, from_=0, to=1, orient="horizontal")
         self.drag_track_conf.set(0.5)
         self.drag_track_conf.place(x= self.BgTop_x+int((self.BgDown_x-self.BgTop_x)*0.1) + width_track_conf, y = self.BgTop_y+int((self.BgDown_y-self.BgTop_y)*0.1+height_weight+height_model_complexity+height_model_conf+16))
         self.drag_track_conf.place_forget()
