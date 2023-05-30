0a71f4d55b43f2d25da072e0a51219b82e86389c
crapper <crapper.612@gmail.com>
2023-05-30 21:12:17 +0800

    refactor: refactor config page name in test_config_page

--- test/test_config_page.py
+++ test/test_config_page.py
@@ -10,41 +10,41 @@ from app import *
 class TestConfigPage(unittest.TestCase):
     def setUp(self):
         self.c = tk.Canvas(None)
-        self.configpage = ConfigPage(self.c, None)
+        self.config_page = ConfigPage(self.c, None)
 
     def tearDown(self):
         pass
 
     def test_show_page(self):
-        self.assertFalse(self.configpage.active)
-        self.configpage.show_page()
-        self.assertTrue(self.configpage.active)
+        self.assertFalse(self.config_page.active)
+        self.config_page.show_page()
+        self.assertTrue(self.config_page.active)
 
     def test_hide_page(self):
-        self.configpage.show_page()
-        self.assertTrue(self.configpage.active)
-        self.configpage.hide_page()
-        self.assertFalse(self.configpage.active)
+        self.config_page.show_page()
+        self.assertTrue(self.config_page.active)
+        self.config_page.hide_page()
+        self.assertFalse(self.config_page.active)
 
     def test_request_open_page(self):
-        self.assertFalse(self.configpage.active)
-        self.configpage.request_open_page()
-        self.assertTrue(self.configpage.active)
-        self.assertTrue(self.configpage.request_open_page())
+        self.assertFalse(self.config_page.active)
+        self.config_page.request_open_page()
+        self.assertTrue(self.config_page.active)
+        self.assertTrue(self.config_page.request_open_page())
     
     def test_request_close_page(self):
-        self.configpage.show_page()
-        self.assertTrue(self.configpage.active)
-        self.configpage.request_close_page()
-        self.assertFalse(self.configpage.active)
-        self.assertTrue(self.configpage.request_close_page())
+        self.config_page.show_page()
+        self.assertTrue(self.config_page.active)
+        self.config_page.request_close_page()
+        self.assertFalse(self.config_page.active)
+        self.assertTrue(self.config_page.request_close_page())
 
     def test_validate_entry(self):
-        self.assertFalse(self.configpage.validate_entry("a"))
-        self.assertTrue(self.configpage.validate_entry("1"))
-        self.assertTrue(self.configpage.validate_entry("1.0"))
-        self.assertTrue(self.configpage.validate_entry("1.0e-10"))
-        self.assertTrue(self.configpage.validate_entry(""))
+        self.assertFalse(self.config_page.validate_entry("a"))
+        self.assertTrue(self.config_page.validate_entry("1"))
+        self.assertTrue(self.config_page.validate_entry("1.0"))
+        self.assertTrue(self.config_page.validate_entry("1.0e-10"))
+        self.assertTrue(self.config_page.validate_entry(""))
     
     
 if __name__ == '__main__':
